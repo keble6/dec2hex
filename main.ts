52076d1cae1bb374b7425e515c919b580a6f2e9f
Rob Wilson <rob@rwilson.org.uk>
2022-07-21 21:02:12 +0100

    debugged

--- main.ts
+++ main.ts
@@ -1,27 +1,11 @@
 let hex2 = 0
-let dec = 18
-let chars = [
-"0",
-"1",
-"2",
-"4",
-"5",
-"6",
-"7",
-"8",
-"9",
-"a",
-"b",
-"c",
-"d",
-"e",
-"f"
-]
-let divisor = 16
+let dec = 43
 let result = ""
+let chars = "0123456789abcdef"
 while (dec != 0) {
     hex2 = dec % 16
-    result = "" + result + chars.charAt(hex2)
+    result = "" + chars.charAt(hex2) + result
     dec = Math.idiv(dec, 16)
 }
-basic.showString("Hello!")
+result = "0x" + result
+basic.showString(result)
